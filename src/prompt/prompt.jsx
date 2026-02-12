c0697a1b812ffd6d6657f7e30b23029a37183cb0
Abigail Peterson <desrange@byu.edu>
2026-02-11 20:53:55 -0700

    Added jsx files for all pages

---
+++ src/prompt/prompt.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Prompt() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Prompt displayed here</div>
+    </main>
+  );
+}
