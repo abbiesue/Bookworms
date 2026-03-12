140600d0af2aa3ef18c8da6422ea1be1d4f9ee1f
Abigail Peterson <desrange@byu.edu>
2026-03-12 15:20:13 -0600

    All endpoints added and ready for frontend

--- service/Response.js
+++ service/Response.js
@@ -4,6 +4,7 @@ class Response {
     text;
     prompt;
     date;
+    timestamp;
     reactions;
     critiques;
 
@@ -11,7 +12,8 @@ class Response {
         this.username = username;
         this.prompt = prompt;
         this.text = text;
-        this.date = new Date();
+        this.date = new Date().toISOString().split('T')[0]
+        this.timestamp = new Date().toISOString();
         this.reactions = new ReactionPanel();
         this.critiques = [];
     }
@@ -21,28 +23,29 @@ class Response {
     getPrompt() { return this.prompt; }
     getText() { return this.text; }
     getDate() { return this.date; }
-    getReactions() { return this.reactions; }
+    getTimestamp() { return this.timestamp; }
+    getReactions(username) { return this.reactions.getReactionData(username); }
     getCritiques() { return this.critiques; }
 
     //updaters
     updateText(text) {
-        const today = new Date().toDateString();
-        if (this.date.toDateString() === today) {
+        const today = new Date().toISOString().split('T')[0];
+        if (this.date === today) {
             this.text = text;
         }
     }
 
-    updateReaction(reactionType, count) {
+    updateReaction(reactionType, user) {
         if (this.reactions) {
             switch (reactionType) {
                 case 'like':
-                    this.reactions.setLikeCount(count);
+                    this.reactions.updateLikes(user);
                     break;
                 case 'laugh':
-                    this.reactions.setLaughCount(count);
+                    this.reactions.updateLaughs(user);
                     break;
                 case 'cry':
-                    this.reactions.setCryCount(count);
+                    this.reactions.updateCries(user);
                     break;
                 default:
                     break;
@@ -65,32 +68,57 @@ class Critique {
     constructor(username, text) {
         this.username = username;
         this.text = text;
-        this.date = new Date();
+        this.date = new Date().toISOString();
     }
 }
 
 class ReactionPanel {
-    likeCount;
-    laughCount;
-    cryCount;
+    likes = [];
+    laughs = [];
+    cries = [];
 
-    constructor(likeCount = 0, laughCount = 0, cryCount = 0) {
-        this.likeCount = likeCount;
-        this.laughCount = laughCount;
-        this.cryCount = cryCount;
+    getReactionData(username) {
+        return {
+            likeCount: this.likes.length,
+            laughCount: this.laughs.length,
+            cryCount: this.cries.length,
+            userReacted: {
+                like: this.likes.includes(username),
+                laugh: this.laughs.includes(username),
+                cry: this.cries.includes(username),
+            }
+        };
     }
 
-    getLikeCount() {return this.likeCount;}
+    getLikeCount() {return this.likes.length;}
 
-    getLaughCount() {return this.laughCount;}
+    getLaughCount() {return this.laughs.length;}
 
-    getCryCount() {return this.cryCount;}
+    getCryCount() {return this.cries.length;}
 
-    setLikeCount(count) {this.likeCount = count;}
+    updateLikes(user) {
+        if (!this.likes.includes(user)) {
+            this.likes.push(user);
+        } else {
+            this.likes = this.likes.filter(u => u !== user);
+        }
+    }
 
-    setLaughCount(count) {this.laughCount = count;}
+    updateLaughs(user) {
+        if (!this.laughs.includes(user)) {
+            this.laughs.push(user);
+        } else {
+            this.laughs = this.laughs.filter(u => u !== user);
+        }
+    }
 
-    setCryCount(count) {this.cryCount = count;}
+    updateCries(user) {
+        if (!this.cries.includes(user)) {
+            this.cries.push(user);
+        } else {
+            this.cries = this.cries.filter(u => u !== user);
+        }
+    }
 }
 
 module.exports = { Response };
--- service/index.js
+++ service/index.js
@@ -9,6 +9,7 @@ const authCookieName = 'token';
 
 //space to store all stuff that will be mapped to a database
 let users = [];
+let responses = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
@@ -19,54 +20,147 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// ---------------ENDPOINTS---------------
+// ---------------LOGIN ENDPOINTS---------------
 //CreateAuth - register a new user
 apiRouter.post('/auth/create', async (req, res) => {
-  if (await findUser('email', req.body.email)) {
-    res.status(409).send({ msg: 'Email already in use' });
-  } else if (await findUser('username', req.body.username)) {
-    res.status(409).send({ msg: 'Username already taken' });
-  } else {
-    const user = await createUser(req.body.username, req.body.email, req.body.password);
-    setAuthCookie(res, user.token);
-    res.send({ username: user.username, email: user.email });
-  }
+    if (await findUser('email', req.body.email)) {
+        res.status(409).send({ msg: 'Email already in use' });
+    } else if (await findUser('username', req.body.username)) {
+        res.status(409).send({ msg: 'Username already taken' });
+    } else {
+        const user = await createUser(req.body.username, req.body.email, req.body.password);
+        setAuthCookie(res, user.token);
+        res.send({ username: user.username, email: user.email });
+    }
 });
 
 //GetAuth - login an existing user
 apiRouter.post('/auth/login', async (req, res) => {
-  const user = await findUser('email', req.body.email);
-  if (user) {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      user.token = uuid.v4();
-      setAuthCookie(res, user.token);
-      res.send({ username: user.username, email: user.email });
-      return;
+    const user = await findUser('email', req.body.email);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+        user.token = uuid.v4();
+        setAuthCookie(res, user.token);
+        res.send({ username: user.username, email: user.email });
+        return;
+        }
     }
-  }
-  res.status(401).send({ msg: 'Invalid username or password' });
+    res.status(401).send({ msg: 'Invalid username or password' });
 });
 
 //DeleteAuth - logout a user
 apiRouter.delete('/auth/logout', async (req, res) => {
-  const user = await findUser('token', req.cookies[authCookieName]);
-  if (user) {
-    delete user.token;
-  }
-  res.clearCookie(authCookieName);
-  res.status(204).end();
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
 });
-// ---------------------------------------
+// ---------------------------------------------
 
 //verifyAuth - verify a user is authorized to call an endpoint
 const verifyAuth = async (req, res, next) => {
-  const user = await findUser('token', req.cookies[authCookieName]);
-  if (user) {
-    next();
-  } else {
-    res.status(401).send({ msg: 'Unauthorized' });
-  }
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
 };
+
+//verifyResponded = verify a user has responded before calling an endpoint
+const verifyResponded = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        const userResponse = getResponse(user.username);
+        if (userResponse) {
+            next();
+        } else {
+            res.status(403).send({ msg: 'User has not responded' });
+        }
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+}
+
+// ------------RESPONSE ENDPOINTS---------------
+//SubmitResponse - should not submit response if user is not logged in
+apiRouter.post('/response/submit', verifyAuth, async (req, res) => {
+    const { prompt, text } = req.body;
+    const user = await findUser('token', req.cookies[authCookieName]);
+    const username = user.username;
+    const response = new Response(username, prompt, text);
+    responses.push(response);
+    res.status(201).send(response);
+});
+
+//EditResponse - endpoint for editing the response text
+apiRouter.put('/response/edit', verifyAuth, verifyResponded, async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    const text = req.body.text;
+    const response = getResponse(user.username);
+    if (response) {
+        const today = new Date().toISOString().split('T')[0];
+        if (response.getDate() !== today) {
+            return res.status(403).send({ msg: 'Cannot edit a previous day\'s response' });
+        }
+        response.updateText(text);
+        res.send(response);
+    } else {
+        res.status(404).send({ msg: 'Response not found' });
+    }
+});
+
+//GetAllResponses - get all stored responses
+apiRouter.get('/response/all', verifyAuth, verifyResponded, async (req, res) => {
+    // send responses back in order they were recieved with the current logged in user's at the top
+    const user = await findUser('token', req.cookies[authCookieName]);
+    const userResponse = getResponse(user.username);
+    const otherResponses = responses
+        .filter((r) => r.username !== user.username)
+        .sort((a, b) => new Date(a.getTimestamp()) - new Date(b.getTimestamp()));
+    res.send(userResponse ? [userResponse, ...otherResponses] : otherResponses);
+});
+
+//GetUserResponse - get the response of the person logged in
+apiRouter.get('/response/user', verifyAuth, async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    const userResponse = getResponse(user.username);
+    if (userResponse) {
+        res.send({ responded: true, response: userResponse });
+    } else {
+        res.send({ responded: false });
+    }
+});
+
+//AddReaction - changes the reaction 
+apiRouter.post('/response/reaction', verifyAuth, verifyResponded,async (req, res) => {
+  const responseAuthor = req.body.responseAuthor;
+  const authorResponse = getResponse(responseAuthor);
+  if (authorResponse) {
+    const reactionType = req.body.reactionType;
+    const user = await findUser('token', req.cookies[authCookieName]);
+    authorResponse.updateReaction(reactionType, user.username);
+    res.status(201).send(authorResponse.getReactions(user.username));
+  } else {
+    res.status(404).send({ msg: 'Response not found' });
+  }
+});
+
+//AddCritique
+apiRouter.post('/response/critique', verifyAuth, verifyResponded, async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    const { responseAuthor, critiqueText } = req.body;
+    const response = getResponse(responseAuthor);
+    if (response) {
+        response.addCritique(user.username, critiqueText);
+        res.send(response.getCritiques());
+    } else {
+        res.status(404).send({ msg: 'Response not found' });
+    }
+});
+// ---------------------------------------------
 
 //basic error handler
 app.use(function (err, req, res, next) {
@@ -77,6 +171,11 @@ app.use(function (err, req, res, next) {
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
+
+//getResponse - get a response by username
+function getResponse(username) {
+  return responses.find((r) => r.username === username);
+}
 
 //createUser
 async function createUser(username, email, password) {
