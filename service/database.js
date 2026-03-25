4b0db6e372fb94c75dc928ba2d8bd3dc0d5bdb06
Abigail Peterson <desrange@byu.edu>
2026-03-25 16:17:53 -0600

    added test connection from the simon project code

--- service/database.js
+++ service/database.js
@@ -11,16 +11,44 @@ const responseCollection = db.collection('responses')
 const archiveCollection = db.collection('archive')
 const bonusCollection = db.collection('bonuses')
 
+// This will asynchronously test the connection and exit the process if it fails (copied from simon project code)
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
 //we will need functions to manipulate each collection
 
 //--------USER FUNCTIONS--------
+//get
+//add
+//update
+//remove
+
 //------------------------------
 
 //------RESPONSE FUNCTIONS------
+//get
+//add
+//update
+//remove
 //------------------------------
 
 //-------ARCHIVE FUNCTIONS------
+//get
+//add
+//update
+//remove
 //------------------------------
 
 //-------BONUSES FUNCTIONS------
+//get
+//add
+//update
+//remove
 //------------------------------
