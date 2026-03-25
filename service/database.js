2d6f75c0a32a980974768859a05b0af8f5b5e608
Abigail Peterson <desrange@byu.edu>
2026-03-25 17:24:32 -0600

    added all functions for response collection manipulation

--- service/database.js
+++ service/database.js
@@ -26,32 +26,83 @@ const bonusCollection = db.collection('bonuses')
 
 //--------USER FUNCTIONS--------
 //get
-function getUser(field, value) {
+function getUser(field, value){
     if (!value) return null;
     return userCollection.findOne({[field] : value});
 }
 
 //add
-async function addUser(user) {
+async function addUser(user){
     await userCollection.insertOne(user);
 }
 
 //update
-async function updateUser(user) {
+async function updateUser(user){
     await userCollection.updateOne({username: user.username}, {$set: user});
 }
 
 //remove
-async function removeUserToken(user) {
+async function removeUserToken(user){
     await userCollection.updateOne({username: user.username},{$unset: {token: 1}});
 }
 //------------------------------
 
 //------RESPONSE FUNCTIONS------
 //get
+function getResponse(username){
+    const today = new Date().toISOString().split('T')[0];
+    return responseCollection.findOne({username, date: today});
+}
+
+async function getAllTodayResponses() {
+    const today = new Date().toISOString().split('T')[0];
+    return responseCollection.find({date: today}).sort({timestamp: 1}).toArray();
+}
+
 //add
+async function addResponse(responseData){
+    await responseCollection.insertOne(responseData);
+}
+
 //update
+async function updateResponseText(username, text){
+    const today = new Date().toISOString().split('T')[0];
+    await responseCollection.updateOne(
+        { username, date: today },
+        { $set: { text, timestamp: new Date().toISOString() } }
+    );
+}
+
+async function updateResponseReaction(username, reactionType, reactingUser) {
+    const today = new Date().toISOString().split('T')[0];
+    const response = await responseCollection.findOne({username, date: today});
+    if (!response) return null;
+    const panel = response.reactions || {likes: [], laugh: [], cries: []};
+    const arrayField = reactionType === 'like' ? 'likes'
+        : reactionType === 'laugh' ? 'laughs'
+        : 'cries';
+
+    if (panel[arrayField].includes(reactingUser)) {
+        panel[arrayField] = panel[arrayField].filter(u => u !== reactingUser);
+    } else {
+        panel[arrayField].push(reactingUser);
+    }
+
+    await responseCollection.updateOne({username, date: today}, {$set: {reactions: panel}});
+    return panel;
+}
+
+async function addCritique(username, critiqueObj) {
+    const today = new Date().toISOString().split('T')[0];
+    await responseCollection.updateOne({username, date: today}, {$push: {critiques: critiqueObj}});
+    const updated = await responseCollection.findOne({ username, dat: today});
+    return updated?.critiques || [];
+}
 //remove
+async function deleteTodayResponses() {
+    const today = new Date().toISOString().split('T')[0];
+    await responseCollection.deleteMany({date: today});
+}
 //------------------------------
 
 //-------ARCHIVE FUNCTIONS------
@@ -67,3 +118,10 @@ async function removeUserToken(user) {
 //update
 //remove
 //------------------------------
+
+module.exports = {
+    getUser, 
+    addUser,
+    updateUser,
+    removeUserToken,
+};
